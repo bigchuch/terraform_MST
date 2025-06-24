e74827f9bb0efbf687a9977a6cfcdf1da9d4bae8
AVMUser-52468038@LODSPRODMCA.onmicrosoft.com <AVMUser-52468038@LODSPRODMCA.onmicrosoft.com>
2025-06-24 06:31:41 -0700

    added locals

--- main.tf
+++ main.tf
@@ -12,12 +12,22 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
 
+locals {
+  prefix = "contoso"
+  region = "North Europe"
+  tags = {
+    cost_center = "contoso research"
+  }
+}
 
 resource "azurerm_resource_group" "contoso_rg" {
-  name     = "contoso_rg"
-  location = "UK South"
-  
-  tags = {
-  cost_center = "contoso research"
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
 }
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
 }
