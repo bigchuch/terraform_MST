2bc13695cb530a043babfa132fd246d831a8450a
bigchuch <eajebon@gmail.com>
2025-06-25 12:36:10 -0700

    Added local module

--- main.tf
+++ main.tf
@@ -16,11 +16,11 @@ provider "azurerm" {
 module "demo" {
   source = "./modules/demo"
 
-  prefix = var.prefix
-  region = var.region
-  resource_groups = var.resource_groups
+  prefix           = var.prefix
+  region           = var.region
+  resource_groups  = var.resource_groups
   virtual_networks = var.virtual_networks
-  tags = var.tags
+  tags             = var.tags
 }
 
 # moved {
